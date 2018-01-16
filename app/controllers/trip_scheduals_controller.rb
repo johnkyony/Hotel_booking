76268906cea87c981c169abcfcb1c68c3adf6a3a
john  kyony <jkyony@gmail.com>
2018-01-16 17:38:33 +0000

    the  status

--- app/controllers/trip_scheduals_controller.rb
+++ app/controllers/trip_scheduals_controller.rb
@@ -3,5 +3,9 @@ class TripSchedualsController < ApplicationController
   end
 
   def new
+    # @client = GooglePlaces::Client.new("AIzaSyAOCgJAr_KwHZvB-oi980QacwgBpBz_ZEw")
+    # @guesthouse_location = Guesthouse.first
+    # @places_feed = @client.spots(@guesthouse_location.latitude , @guesthouse_location.longitude , :types => ["point_of_interest"])
   end
+  
 end
--- app/controllers/user_home_section_controller.rb
+++ app/controllers/user_home_section_controller.rb
@@ -4,6 +4,6 @@ class UserHomeSectionController < ApplicationController
     @user_bookings = current_user.bookings
     @client = GooglePlaces::Client.new("AIzaSyAOCgJAr_KwHZvB-oi980QacwgBpBz_ZEw")
     @guesthouse_location = Guesthouse.first
-    @places_feed = @client.spots(@guesthouse_location.latitude , @guesthouse_location.longitude)
+    @places_feed = @client.spots(@guesthouse_location.latitude , @guesthouse_location.longitude , :types => ['restaurant'])
   end
 end
